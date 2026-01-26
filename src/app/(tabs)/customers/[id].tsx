0f705ddc9e7451fe9c239d9205821da6b3f46f5a
Can Nasıf <can.nasif@v3rii.com>
2026-01-26 22:58:01 +0300

    fix: Fix customer detail page import
    - Change CustomerDetailPage import from named to default export
    - Fix TypeError: Cannot read property 'displayName' of undefined

--- src/app/(tabs)/customers/[id].tsx
+++ src/app/(tabs)/customers/[id].tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { CustomerDetailPage } from "./_components/CustomerDetailPage";
+import CustomerDetailPage from "./_components/CustomerDetailPage";
 
 export default function CustomerDetailRoute(): React.ReactElement {
   return <CustomerDetailPage />;
